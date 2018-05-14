ae3a5feaae29d34ce18202119aa599a9e0ab05ad
Ben-Perlin <baperlin@gmail.com>
2018-05-14 17:35:01 -0500

    fixed windows matrix_precision

--- matrix_precision.m
+++ matrix_precision.m
@@ -5,19 +5,27 @@ function value = matrix_precision(A)
 % "mean_manhatten"
 %
 % The code for this and the subprogram to create it could be converted
-% to produce a histogram of distances if needed 
+% to produce a histogram of distances if needed
 % Written by Ben Perlin 2018
 
     filename = tempname;                    % generate a unique temporary filename
     csvwrite(filename, logical(A));         % write data to that file
-    [status, cmdout] = system(['./mean_manhatten ', filename]); % execute command
     
-    delete filename;
-    
-    if (status ~= 0) 
-        error(['program "mean_manhatten" failed: ',  cmdout]);
+    if ispc
+        cmd = 'mean_manhatten '; % drop ./
+    else
+        cmd = './mean_manhatten ';
     end
     
+    [status, cmdout] = system([cmd, filename]); % execute command
+
+    %delete filename; % doesn't work, but files should be auto deleted from
+    %tmp
+
+    if (status ~= 0)
+        error(['program "mean_manhatten" failed: ',  cmdout]);
+    end
+
     % todo parse
     value = str2double(cmdout);
 end
